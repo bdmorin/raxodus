e2de695f709f62bbc03ad5dfd9e7f3c6ec09c5d7
Brian Morin <bmorin@provisioniam.com>
2025-08-06 02:23:19 -0500

    chore: Bump version to 0.1.2 for testing GitHub Actions PyPI publish

--- src/raxodus/version.py
+++ src/raxodus/version.py
@@ -1,6 +1,6 @@
 """Version and release information for raxodus."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __codename__ = "Mondain"
 __tagline__ = "The dark wizard whose defeat marked the beginning of the Age of Darkness"
 
